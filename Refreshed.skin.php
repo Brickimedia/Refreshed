fb7fb8bae8fa2fcdae9f824b1ffb41ab5e2533a9
MtMNC <MtMNC@thisisnotarealdomain.thisisnotarealtld>
2015-06-12 15:22:39 -0700

    (Hopefully) fixing WikiFont in IE per Brickimedia/brickimedia#382

--- Refreshed.skin.php
+++ Refreshed.skin.php
@@ -69,6 +69,7 @@ class SkinRefreshed extends SkinTemplate {
 		// Internet Explorer fixes
 		$out->addStyle( $wgStylePath . '/Refreshed/refreshed/ie8.css', 'screen', 'IE 8' );
 		$out->addStyle( $wgStylePath . '/Refreshed/refreshed/iefontfix.css', 'screen', 'IE' );
+		$out->addStyle( $wgStylePath . '/Refreshed/refreshed/wikifont/WikiFontLoader.css', 'screen and (-ms-high-contrast: active), (-ms-high-contrast: none)' ); /* IE 10+ */
 	}
 }
 
