1ed543ce8ff44c27e59e104724702099b24c5f8e
MtMNC <MtMNC@thisisnotarealdomain.thisisnotarealtld>
2015-08-10 10:39:10 -0700

    Testing fonts in Edge
    See Brickimedia/brickimedia#408

--- Refreshed.skin.php
+++ Refreshed.skin.php
@@ -69,7 +69,7 @@ class SkinRefreshed extends SkinTemplate {
 		// Internet Explorer fixes
 		$out->addStyle( $wgStylePath . '/Refreshed/refreshed/ie8.css', 'screen', 'IE 8' );
 		$out->addStyle( $wgStylePath . '/Refreshed/refreshed/iefontfix.css', 'screen', 'IE' );
-		$out->addStyle( $wgStylePath . '/Refreshed/refreshed/wikifont/WikiFontLoader.css', 'screen and (-ms-high-contrast: active), (-ms-high-contrast: none)' ); /* IE 10+ */
+		$out->addStyle( $wgStylePath . '/Refreshed/refreshed/wikifont/WikiFontLoader.css', 'screen' ); /* IE 10+ */
 	}
 }
 
