f4166dc0759c716b5573694192a912c949d14811
UltrasonicNXT <adamr_carter@btinternet.com>
2016-01-09 17:43:26 +0000

    add toolbox dropdown

--- refreshed/refreshed.js
+++ refreshed/refreshed.js
@@ -122,10 +122,10 @@ $( document ).ready( function() {
 	});
 	
 	// working code for dropdowns. Note: sinple code like this is much better than complicated like below :)
-	$( "a.header-button.fade-trigger" ).click( function( e ) {
+	$( "a.fade-trigger" ).click( function( e ) {
 		Refreshed.toggleFade( $( e.target ).parent() );
 	} );
-	
+
 	$( "a.sidebar-shower" ).click( function( e ) {
 		Refreshed.toggleSidebar();
 	} );
